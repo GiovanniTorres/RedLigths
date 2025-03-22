eb5caf4e74b939f4743d067fbe3decf4565ef734
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-22 15:37:55 -0600

    Actualizar index.php

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
-class Persona {}
+class Persona{
+public $nombre;
+public $apellido;
+public $edad;
+
+public function __construct($nombre, $apellido, $edad){
+$this->nombre = $nombre;
+$this->apellido = $apellido;
+$this->edad = $edad;
+}
+
+public function showMe(){
+return "Nombre: ".$this->nombre.", Apellido: ".$this->apellido.", Edad: ".$this->edad;
+}
+}
+
+$persona1 = new Persona("Samahí","Hernandez","Rosas");
+print $persona1->showMe();
