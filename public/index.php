57b6e4f2197c498f241d22bcfd64c5173f5404a9
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-29 16:42:22 -0600

    Actualizar index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,7 @@
-<?php
-error_reporting(E_ALL); //Útiles en desarrollo 
-ini_set('display_errors', 1); // Útiles en desarrollo 
-
-// Autocargar clases automáticamente
 spl_autoload_register(function ($class) {
     $file = __DIR__ . '/../' . str_replace('\\', '/', $class) . '.php';
-
-  echo $file;
-  var_dump($file); // Muestra información detallada sobre la variable
+    
+    echo $file; // Imprime la ruta generada
+    // o
+    var_dump($file); // Muestra información detallada sobre la variable
 });
-
