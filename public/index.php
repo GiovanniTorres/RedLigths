a6946762aaf06c9bd5d5fdba73bfb1d271e5e4f7
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-29 17:21:23 -0600

    Actualizar index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
+error_reporting(E_ALL); //Útiles en desarrollo 
+ini_set('display_errors', 1); // Útiles en desarrollo 
+
+// Autocargar clases automáticamente
 spl_autoload_register(function ($class) {
     $file = __DIR__ . '/../' . str_replace('\\', '/', $class) . '.php';
     
-    echo $file; // Imprime la ruta generada
-    // o
-    var_dump($file); // Muestra información detallada sobre la variable
+    if (file_exists($file)) {
+        require_once $file;
+    } else {
+        die("Error: No se pudo cargar la clase $class (Archivo esperado: $file)");
+    }
 });
