b1eb06ad45f9fcb88b90a413ef6aecf42dbb92b6
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-30 11:40:49 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once __DIR__ . '/../core/Autoload.php'; // Cargar el autoloader
 require_once __DIR__ . '/../core/Router.php'; // Cargar el enrutador
 
