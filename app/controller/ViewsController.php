e5538d65164cfa3396b146e86188ad305f79d17c
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-30 15:34:11 -0600

    Update ViewsController.php

--- app/controller/ViewsController.php
+++ app/controller/ViewsController.php
@@ -5,12 +5,12 @@ class ViewsController
 {
     public function view($view)
     {
-        $file = "../app/views/{$view}.php";
+        $file = __DIR__ . "/../../app/views/{$view}.php";
         
         if (file_exists($file)) {
-            require_once '../app/views/header.php';
+            require_once __DIR__ . '/../../app/views/header.php';
             require_once $file;
-            require_once '../app/views/footer.php';
+            require_once __DIR__ . '/../../app/views/footer.php';
         } else {
             echo "Error 404: Vista no encontrada.";
         }
