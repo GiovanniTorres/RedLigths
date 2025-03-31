6be9b8990196a2a5303ec7f1b712a1b98440428d
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-30 23:04:50 -0600

    Update header.php

--- app/views/header.php
+++ app/views/header.php
@@ -1,2 +1,40 @@
-<?php
-print "header";
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>redLigths()</title>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <link rel="stylesheet" href="menu.css"> <!-- Menú Estilos -->
+    <link rel="stylesheet" href="style.css"> <!-- Estilos Generales -->
+   <link rel="stylesheet" href="home.css"> <!-- Estilos Generales -->
+
+   <link rel="stylesheet" href="footer.css"> <!-- Estilos Generales -->
+
+<script src="menu.js"></script>
+
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <div class="logo">
+                <a href="#">redLigths&nbsp<span id="parentesis">()</span></a>
+            </div>
+            <nav class="navbar">
+                <ul class="menu" id="menu">
+                    <li><a href="?views=home">Inicio</a></li>
+                    <li><a href="?views=product">Productos</a></li>
+                    <li><a href="?views=contact">Contacto</a></li>
+                    <li><a href="?views=about">Sobre Nosotros</a></li>
+                </ul>
+                <div class="menu-toggle" id="menu-toggle">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+            </nav>
+        </div>
+    </header>
