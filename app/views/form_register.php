08ebc05d256eada779dcd0b0c9b4104dd6f15f1b
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-01 16:06:58 -0600

    Update form_register.php

--- app/views/form_register.php
+++ app/views/form_register.php
@@ -13,7 +13,7 @@ $data = require __DIR__ . '/auth/register.php';
 
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
         <form method="POST" action="" class="bg-white p-6 rounded-lg shadow-md w-96">
-            <h2 class="text-center text-xl font-bold mb-4">Iniciar Sesión</h2>
+            <h2 class="text-center text-xl font-bold mb-4">Registrate</h2>
             <input type="text" class="block w-full-mb-2 p-2 border rounded" name="name" placeholder="Nombre" required>
             <input type="email" class="block w-full-mb-2 p-2 border rounded" name="email" placeholder="Correo" required>
             <input type="password" class="block w-full-mb-2 p-2 border rounded" name="password" placeholder="Contraseña" required>
