d371251829f13242a8e977870fbc224738ad9773
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-01 23:17:52 -0600

    Update form_register.php

--- app/views/form_register.php
+++ app/views/form_register.php
@@ -14,9 +14,9 @@ $data = require __DIR__ . '/auth/register.php';
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
         <form method="POST" action="" class="bg-white p-6 rounded-lg shadow-md w-96">
             <h2 class="text-center text-xl font-bold mb-4">Registrate</h2>
-            <input type="text" class="block w-full-mb-2 p-2 border rounded" name="name" placeholder="Nombre" required>
-            <input type="email" class="block w-full-mb-2 p-2 border rounded" name="email" placeholder="Correo" required>
-            <input type="password" class="block w-full-mb-2 p-2 border rounded" name="password" placeholder="Contraseña" required>
+            <input type="text" class="block w-full mb-2 p-2 border rounded" name="name" placeholder="Nombre" required>
+            <input type="email" class="block w-full mb-2 p-2 border rounded" name="email" placeholder="Correo" required>
+            <input type="password" class="block w-full mb-2 p-2 border rounded" name="password" placeholder="Contraseña" required>
             <button type="submit" class="w-full bg-blue-500 text-white p-2" >Registrar</button>
         </form>
     </div>
