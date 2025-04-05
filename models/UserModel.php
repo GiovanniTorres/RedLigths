db85a765585c24dfbdb170dfbb244c032ae8113c
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-05 17:16:53 -0600

    Update UserModel.php

--- models/UserModel.php
+++ models/UserModel.php
@@ -4,11 +4,15 @@ require_once __DIR__ . '/../Core/Database.php';
 class UserModel {
     private $db;
 
-        public function __construct() {
-                $this->db = new Database();
-                    }
+    public function __construct() {
+        $database = new Database();
+        $this->db = $database->getConnection();
+    }
 
-                        public function getAllUsers() {
-                                return $this->db->get("SELECT * FROM users");
-                                    }
-                                    }
+    public function getAll() {
+        $stmt = $this->db->query("SELECT * FROM users");
+        return $stmt->fetchAll();
+    }
+
+    // Aquí luego añadimos create, update, delete
+}
