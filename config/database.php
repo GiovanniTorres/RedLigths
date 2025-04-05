1f7eb84ff8e302d7dd00e6e7f87a51933f748fe9
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-05 17:13:47 -0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
-
 <?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'RedLights');
-define('DB_USER', 'root');
-define('DB_PASS', 'samahiHernandez(0)'); // Si tienes contraseña, agrégala aquí
+return [
+    'host' => 'aws.connect.psdb.cloud',
+    'user' => 's04vr2uceeclmt1byk2w',
+    'pass' => 'pscale_pw_mQ4sRZGgfXXhVAapcvDXmmjKmK61FcwQYSbThZNtUhK',
+    'name' => 'redlights'
+];
