b418f162331ceafdca57697fd1565351836c7220
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-05 17:18:28 -0600

    Update UserModel.php

--- models/UserModel.php
+++ models/UserModel.php
@@ -14,5 +14,24 @@ class UserModel {
         return $stmt->fetchAll();
     }
 
-    // Aquí luego añadimos create, update, delete
+    public function getById($id) {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+
+    public function create($data) {
+        $stmt = $this->db->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+        return $stmt->execute([$data['username'], $data['email'], $data['password']]);
+    }
+
+    public function update($id, $data) {
+        $stmt = $this->db->prepare("UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?");
+        return $stmt->execute([$data['username'], $data['email'], $data['password'], $id]);
+    }
+
+    public function delete($id) {
+        $stmt = $this->db->prepare("DELETE FROM users WHERE id = ?");
+        return $stmt->execute([$id]);
+    }
 }
