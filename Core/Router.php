60aea945a976df30164f0278bcc2222619fdf92c
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-07 10:14:34 -0600

    Update Router.php

--- Core/Router.php
+++ Core/Router.php
@@ -16,7 +16,7 @@ class Router
                 break;
 
             // Usuarios (vistas dinámicas)
-            case '/users':
+            case '/registered_users':
                 $controllerClass = "App\\Controller\\UserController";
                 break;
 
