306c2a36f027074322a4599ce0a53d04f9cfae1c
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-08 12:37:07 -0600

    Update Router.php

--- Core/Router.php
+++ Core/Router.php
@@ -9,7 +9,7 @@ class Router
     {
         // Obtener la URI de la solicitud desde el parámetro 'url'
         $uri = isset($_GET['url']) ? $_GET['url'] : '/';
-        print $_GET['url'] ;
+        print $_GET["url"] ;
         print "Ruta: " . $uri . "<br>";
 
         // Definir las rutas y sus controladores/métodos
