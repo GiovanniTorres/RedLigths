5ba45d0cb0eee689eda06a8210214c5d3b93890e
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-09 17:58:05 -0600

    Update Router.php

--- Core/Router.php
+++ Core/Router.php
@@ -19,7 +19,7 @@ class Router
 
         if ($controllerName === 'aboutme') {
             $controller = new \App\Controller\ViewsController();
-            $controller->aboutme();
+            $controller->index();
             return;
         }
 
