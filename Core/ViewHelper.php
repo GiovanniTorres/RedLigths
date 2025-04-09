e5b10a6bf6e908b77e562c5678ddf5128c19ad85
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-09 13:47:22 -0600

    Update ViewHelper.php

--- Core/ViewHelper.php
+++ Core/ViewHelper.php
@@ -8,14 +8,14 @@ class ViewHelper
         extract($data); // Extrae variables para usar en la vista
 
         // Rutas absolutas usando __DIR__ para evitar problemas de rutas relativas
-        $viewFile = __DIR__ . "/../app/views/{$view}.php";
-        //$header = __DIR__ . "/../app/views/header.php";
+        //$viewFile = __DIR__ . "/../app/views/{$view}.php";
+        $header = __DIR__ . "/../app/views/header.php";
         //$footer = __DIR__ . "/../app/views/footer.php";
 
         if (file_exists($viewFile)) {
             require_once $header;
-            require_once $viewFile;
-            require_once $footer;
+            //require_once $viewFile;
+            //require_once $footer;
         } else {
             echo "<h1>404 - La vista '$view' no fue encontrada</h1>";
         }
