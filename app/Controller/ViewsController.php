dabedb2429c4884cd2e1b5336c651e9059312bd5
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-09 13:13:04 -0600

    Update ViewsController.php

--- app/Controller/ViewsController.php
+++ app/Controller/ViewsController.php
@@ -1,21 +1,17 @@
 <?php
 namespace App\Controller;
+
 use Core\ViewHelper;
 
 class ViewsController
 {
     public function index()
     {
-        //print "ViewsController";
         ViewHelper::render('home');
     }
 
-    public function product($id = null)
+    public function contact()
     {
-        if ($id) {
-            print "Producto con ID: $id";
-        } else {
-            print "Lista de productos";
-        }
+        ViewHelper::render('contact');
     }
 }
