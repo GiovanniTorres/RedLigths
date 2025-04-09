8ddd5d23a06d410b4d86a9fdb48dff51662dbe0d
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-09 16:10:15 -0600

    Update ViewsController.php

--- app/Controller/ViewsController.php
+++ app/Controller/ViewsController.php
@@ -8,7 +8,7 @@ class ViewsController
     public function index()
     {
         // Simplemente imprimir el mensaje
-        echo "Llamando al render desde el controlador 'index'<br>";
+        //echo "Llamando al render desde el controlador 'index'<br>";
         ViewHelper::render('home');  // Llamamos al render con la vista 'home'
     }
 }
