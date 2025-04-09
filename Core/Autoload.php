cf8a70e234f91d639cfbaf5823e47b41b956bcc5
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-09 14:31:45 -0600

    Update Autoload.php

--- Core/Autoload.php
+++ Core/Autoload.php
@@ -2,21 +2,25 @@
 // Core/Autoload.php
 
 spl_autoload_register(function ($class) {
-    $prefix = 'App\\';
-    $base_dir = __DIR__ . '/../app/';
+    $prefixes = [
+        'App\\' => __DIR__ . '/../app/',
+        'Core\\' => __DIR__ . '/../Core/'
+    ];
 
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
+    foreach ($prefixes as $prefix => $base_dir) {
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            continue;
+        }
 
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+        $relative_class = substr($class, $len);
+        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
-    if (file_exists($file)) {
-        require_once $file;
-    } else {
-        echo "No se pudo cargar la clase: $class <br>";
-        echo "Buscado en: $file";
+        if (file_exists($file)) {
+            require_once $file;
+        } else {
+            echo "No se pudo cargar la clase: $class <br>";
+            echo "Buscado en: $file";
+        }
     }
 });
