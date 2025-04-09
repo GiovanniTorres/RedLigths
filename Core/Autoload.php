3b6b3376f290daa0b4378a426c9ffaeb3c6f1ba6
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-09 01:06:10 -0600

    Update Autoload.php

--- Core/Autoload.php
+++ Core/Autoload.php
@@ -1,13 +1,22 @@
 <?php
+// Core/Autoload.php
 
 spl_autoload_register(function ($class) {
-    // Reemplaza backslashes por slashes y ajusta el path
-    $classPath = str_replace('\\', '/', $class);
-    $file = __DIR__ . '/../' . $classPath . '.php';
+    $prefix = 'App\\';
+    $base_dir = __DIR__ . '/../app/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
     if (file_exists($file)) {
         require_once $file;
     } else {
-        echo "No se pudo cargar la clase: $class";
+        echo "No se pudo cargar la clase: $class <br>";
+        echo "Buscado en: $file";
     }
 });
