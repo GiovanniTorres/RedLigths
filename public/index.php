afbbe5e5d239809ca626d14179cfd82ad3ab08e4
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-09 00:53:59 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
 <?php
-
-require_once __DIR__ . '/../Core/Autoload.php'; // Cargar el autoloader  
-require_once __DIR__ . '/../Core/Router.php'; // Cargar el enrutador
+require_once __DIR__ . '/../Core/Autoload.php';
+require_once __DIR__ . '/../Core/Router.php';
 
 use Core\Router;
 
-// Crear una instancia del enrutador y manejar la solicitud
 $router = new Router();
-$router->handleRequest(); // Gestionar la solicitud y cargar la vista correspondiente
+$router->handleRequest();
