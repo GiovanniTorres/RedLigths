d8b7c0cad6abeb78f81cb2e7e6a52dabfa095963
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-09 21:34:01 -0600

    Update ViewsController.php

--- app/Controller/ViewsController.php
+++ app/Controller/ViewsController.php
@@ -8,23 +8,25 @@ class ViewsController
     public function index()
     {
         // Pasar un dato de prueba a la vista
-        $data = ['title' => 'Bienvenido a la página de inicio'];
+        //$data = ['title' => 'Bienvenido a la página de inicio'];
         
         // Llamar al render
-        ViewHelper::render('home', $data);
+        ViewHelper::render('home', 
+                           //$data
+                          );
     }
 
     public function aboutme()
     {
         \Core\ViewHelper::render('aboutme', [
-            'title' => 'Sobre mí'
+            //'title' => 'Sobre mí'
         ]);
     }
 
     public function contact()
     {
         \Core\ViewHelper::render('contact', [
-            'title' => 'Contacto'
+            //'title' => 'Contacto'
         ]);
     }
 }
