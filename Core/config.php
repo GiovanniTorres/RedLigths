30f1630aa863b601e5791b3f10150333f1fea895
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-11 18:18:27 +0000

    data

--- Core/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'redlights');
-define('DB_USER', 'root');
-define('DB_PASS', 'samahiHernandez(0)'); // Si tienes contraseña, agrégala aquí
