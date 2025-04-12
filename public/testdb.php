df46a265cc4358edd7307844969ec12b2a8dff11
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-12 06:40:17 +0000

    data

---
+++ public/testdb.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+
+$db = new Database();
+$conn = $db->getConnection();
+
+if ($conn) {
+    echo "¡Conexión exitosa a PlanetScale con PDO!";
+    }
