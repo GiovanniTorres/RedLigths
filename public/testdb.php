24fb200f98efe4cf793996a9f7d2c181d9ea6a79
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-13 11:40:40 -0600

    Update testdb.php

--- public/testdb.php
+++ public/testdb.php
@@ -1,9 +1,9 @@
 <?php
 require_once __DIR__ . '/../config/database.php';
 
-$db = new Database();
+$db = new Database(); // usa SQLite por default
 $conn = $db->getConnection();
 
 if ($conn) {
-    echo "¡Conexión exitosa a PlanetScale con PDO!";
+    echo "¡Conexión exitosa con SQLite!";
 }
