ef733a51494d34aa916cf0a556eead3efe5001b5
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-14 17:13:24 -0600

    Update NewsletterSubscriberModel.php

--- model/NewsletterSubscriberModel.php
+++ model/NewsletterSubscriberModel.php
@@ -1,1 +1,18 @@
+<?php
+class NewsletterSubscriberModel {
+    private $db;
 
+    public function __construct($conn) {
+        $this->db = $conn;
+    }
+
+    public function subscribe($email) {
+        $stmt = $this->db->prepare("INSERT INTO newsletter_subscribers (email) VALUES (?)");
+        return $stmt->execute([$email]);
+    }
+
+    public function getAllSubscribers() {
+        $stmt = $this->db->query("SELECT * FROM newsletter_subscribers");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
