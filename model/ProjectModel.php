3e88d8cf1b1f44087798e4388bd101c941268167
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-14 15:57:00 -0600

    Update ProjectModel.php

--- model/ProjectModel.php
+++ model/ProjectModel.php
@@ -1,18 +1,24 @@
 <?php
 class ProjectModel {
-    private $conn;
+    private $db;
 
-        public function __construct($db) {
-                $this->conn = $db;
-                    }
+    public function __construct($conn) {
+        $this->db = $conn;
+    }
 
-                        public function create($user_id, $title) {
-                                $stmt = $this->conn->prepare("INSERT INTO projects (user_id, title) VALUES (?, ?)");
-                                        return $stmt->execute([$user_id, $title]);
-                                            }
+    public function createProject($userId, $title, $description) {
+        $stmt = $this->db->prepare("INSERT INTO projects (user_id, title, description) VALUES (?, ?, ?)");
+        return $stmt->execute([$userId, $title, $description]);
+    }
 
-                                                public function getAll() {
-                                                        $stmt = $this->conn->query("SELECT * FROM projects");
-                                                                return $stmt->fetchAll();
-                                                                    }
-                                                                    }
+    public function getAllProjects() {
+        $stmt = $this->db->query("SELECT * FROM projects");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getProjectsByUser($userId) {
+        $stmt = $this->db->prepare("SELECT * FROM projects WHERE user_id = ?");
+        $stmt->execute([$userId]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
