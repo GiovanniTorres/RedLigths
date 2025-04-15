2ee327c7a81f1fb9c7764e6b7e763bd42c8abb45
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-15 14:24:14 -0600

    Update init_db.php

--- config/init_db.php
+++ config/init_db.php
@@ -1,12 +1,13 @@
 <?php
-$dbFile = __DIR__ . '/local.db';
-$schemaFile = __DIR__ . '/schema.sql';
+$dbFile = __DIR__ . '/../Core/local.db';
+$schemaFile = __DIR__ . '/../config/schema.sql';
 
 try {
     $pdo = new PDO('sqlite:' . $dbFile);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $schema = file_get_contents($schemaFile);
+    $pdo->exec("PRAGMA foreign_keys = ON;");
     $pdo->exec($schema);
 
     echo "Base de datos inicializada correctamente.";
