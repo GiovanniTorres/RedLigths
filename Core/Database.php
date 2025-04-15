d23a57dede15cf9f9b8206efd6a05d57a10e551b
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-15 09:39:08 -0600

    Create Database.php

---
+++ Core/Database.php
@@ -0,0 +1,22 @@
+<?php
+class Database {
+    private $conn;
+
+    public function __construct() {
+        $this->connect();
+    }
+
+    private function connect() {
+        try {
+            $this->conn = new PDO("sqlite:" . __DIR__ . "/../local.db");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("PRAGMA foreign_keys = ON;"); // Muy importante
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
