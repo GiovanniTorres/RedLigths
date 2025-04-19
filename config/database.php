2c43949dbd664136c22a2386b0bde687126b3319
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-19 14:45:02 -0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@ class Database {
 
     private function connect() {
         try {
-            $this->conn = new PDO("sqlite:" . __DIR__ . "/../local.db");
+            $this->conn = new PDO("sqlite:" . __DIR__ . "/../Core/local.db");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->exec("PRAGMA foreign_keys = ON;"); // Muy importante
         } catch (PDOException $e) {
